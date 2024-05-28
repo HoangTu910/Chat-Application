0ac0063948d7f08b11dbebceb4a35ec293afcd3d
Hoang Huu Tu <101467222+HoangTu910@users.noreply.github.com>
2024-05-28 16:34:00 +0700

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -13,7 +13,7 @@ void GENERATE_LOGIN(){
 	int height = 10;
 	int top = 7;
 	int left = centerWindow(width);
-    	std::string checkResponse = "200";
+    std::string checkResponse = "200";
 	do {
 		std::string chatLabel = "CHAT APPLICATION";
 		std::string loginLabel = "LOGIN";
@@ -37,20 +37,17 @@ void GENERATE_LOGIN(){
 		std::getline(std::cin>>std::ws, client.username);
 		gotoXY(left + 16, top + 6);
 		std::getline(std::cin>>std::ws, client.password);
-	//Combine message and key to send
         std::string message = client.username + "," + client.password + ",1";
-        //Send message with key to server
+        
         send(clientSocket, message.c_str(), message.length(), 0);
         char buffer[1024] = {0};
-	//Receive response from server and handle specific task for clients
         int recvSize = recv(clientSocket, buffer, sizeof(buffer), 0);
         if (recvSize > 0){
             std::string response(buffer, recvSize);
             checkResponse = response;
         } 
-    }while(checkResponse != "201"); //check validate, retype username and password when !validate
+    }while(checkResponse != "201"); //check validate
     notiBox("Now you can start the conversation");
-    //Start the chat
     return JOIN_CHAT();
 }
 
@@ -58,14 +55,14 @@ void GENERATE_SIGNUP(){
     system("cls");
 	hideCursor(false);
 	std::string newPassword = "/";
-	std::string username;
+	std::string check;
 	std::string warnLabel = "Welcome user !";
 	std::string continueNoti = "Press 'r' to continue...";
 	int width = 45;
 	int height = 9;
 	int top = 7;
 	int left = centerWindow(width);
-	while (client.password != newPassword) {
+	while (client.password != newPassword || check != "200") {
 		std::string chatLabel = "CHAT APPLICATION";
 		std::string loginLabel = "SIGN UP";
 
@@ -93,30 +90,56 @@ void GENERATE_SIGNUP(){
 		gotoXY(left + 23, top + 6);
 		std::cin >> newPassword;
 
-		warnLabel = "Password does not match";
+        if(client.password != newPassword){
+            warnLabel = "Password does not match";
+            continue;
+        }
+
+        std::string message = client.username + "," + client.password + ",0";
+        send(clientSocket, message.c_str(), message.length(), 0);
+        char buffer[1024] = {0};
+        int recvSize = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (recvSize > 0) {
+            std::string response(buffer, recvSize);
+            check = response;
+            if (response == "200") {
+                notiBox("Sign up completed");
+            } 
+            else if(check == "403"){
+                warnLabel = "Username is already existed";
+                continue;
+            }
+            else {
+                notiBox("Something wrong with server");
+            }
+        }
 	}
 	//global.employee.setEmployeePassword(newPassword);
     //Set password and login name for user here
-    std::string message = client.username + "," + client.password + ",0";
-    send(clientSocket, message.c_str(), message.length(), 0);
-	system("cls");
+    // std::string message = client.username + "," + client.password + ",0";
+    // send(clientSocket, message.c_str(), message.length(), 0);
+	// system("cls");
 	
-    char buffer[1024] = {0};
-    int recvSize = recv(clientSocket, buffer, sizeof(buffer), 0);
-    if (recvSize > 0) {
-        std::string response(buffer, recvSize);
-        if (response == "200") {
-            notiBox("Sign up completed");
-        } else {
-            notiBox("Something wrong with server");
-        }
-    }
+    // char buffer[1024] = {0};
+    // int recvSize = recv(clientSocket, buffer, sizeof(buffer), 0);
+    // if (recvSize > 0) {
+    //     std::string response(buffer, recvSize);
+    //     if (response == "200") {
+    //         notiBox("Sign up completed");
+    //     } 
+    //     else if(response == "403"){
+    //         notiBox("Username is already existed");
+    //     }
+    //     else {
+    //         notiBox("Something wrong with server");
+    //     }
+    // }
     return GENERATE_LOGIN();
 }
 
 void receiveMessages(SOCKET clientSocket) {
     char buffer[4096]; // Tăng kích thước buffer lên 4096 byte
-    std::string label = "============ ROOM CHAT 1 ============";
+    std::string label = "====================== ROOM CHAT 1 ======================";
     while (true) {
         int recvSize = recv(clientSocket, buffer, sizeof(buffer), 0);
         gotoXY(centerWindow(label.length()), ++endLine);
@@ -124,7 +147,7 @@ void receiveMessages(SOCKET clientSocket) {
             std::string message(buffer, recvSize);
             std::cout << message << std::endl;
         }
-        gotoXY(22, 26);
+        gotoXY(22, 2);
         std::cout << "You: ";
     }
 }
@@ -132,7 +155,6 @@ void receiveMessages(SOCKET clientSocket) {
 
 void FIRST_MENU()
 {
-    //Choose between login and signup
     int option = optionMenu(8, "CHAT APPLICATION", "LOGIN OR SIGN UP", 2, "1. Login", "2. Sign up");
     switch(option){
         case 1: GENERATE_LOGIN(); break;
@@ -140,7 +162,6 @@ void FIRST_MENU()
     }
 }
 
-//Set up SOCKET
 int SOCKET_START(SOCKET &clientSocket)
 {
     WSADATA wsaData;
@@ -186,35 +207,42 @@ void RESET_SOCKET()
 
 void JOIN_CHAT()
 {
+    hideCursor(true);
     int width = 80;
 	int height = 1;
-	int top = 25;
+	int top = 1;
 	int left = centerWindow(width);
-    drawRectangle(left, top, width, height);
-    std::string label = "============ ROOM CHAT 1 ============";
+    char key;
+    std::string label = "====================== ROOM CHAT 1 ======================";
     std::thread receiveThread(receiveMessages, clientSocket);
     receiveThread.detach();
     gotoXY(centerBox(width, label.length()), 4);
     std::cout << label;
     while (true) {
-        gotoXY(22, 26);
-        std::string msg;
-        std::cout << "You: ";
-        std::getline(std::cin >>std::ws, msg);
-        gotoXY(27, 26); // Move cursor to the beginning of the input line
-        for (size_t i = 0; i < msg.length() + 5; ++i) { // +5 to cover "You: " prefix
-            std::cout << " "; // Overwrite with spaces
+        key = _getch();
+        drawRectangle(left, top, width, height);
+        if(key == 't' || key == 'T'){
+            hideCursor(false);
+            gotoXY(22, 2);
+            std::string msg;
+            std::cout << "You: ";
+            std::getline(std::cin >>std::ws, msg);
+            gotoXY(22, 2); // Move cursor to the beginning of the input line
+            for (size_t i = 0; i < msg.length() + 5; ++i) { // +5 to cover "You: " prefix
+                std::cout << " "; // Overwrite with spaces
+            }
+            //Move cursor back to the beginning of the input line
+            gotoXY(27, 2);
+            gotoXY(centerWindow(label.length()), ++endLine);
+            std::cout << "You: " << msg;
+            send(clientSocket, msg.c_str(), msg.length(), 0);
         }
-
-        // Move cursor back to the beginning of the input line
-        gotoXY(27, 26);
-        gotoXY(centerWindow(label.length()), ++endLine);
-        std::cout << "You: " << msg;
-        send(clientSocket, msg.c_str(), msg.length(), 0);
+        removeRectangle(left, top, width, height);
     }
 }
 
 int main(){
+    WINDOW_RESOLUTION();
     SET_UP_CONNECTION();
     FIRST_MENU();
 }
